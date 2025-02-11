44dc5c43e2a57a9b560c33bec1b59c2908ef5b2a
Gariman <garimanblack1@gmail.com>
2025-02-11 16:45:00 +0200

    Solution Fix

--- task.ps1
+++ task.ps1
@@ -1,7 +1,7 @@
-$resourseGroup = "mate-azure-task-5"
+$resourceGroup = "mate-azure-task-5"
 $resultOut = "$PSScriptRoot\result.json"
 
-$diskVM = Get-AzDisk -ResourceGroupName $resourseGroup
+$diskVM = Get-AzDisk -ResourceGroupName $resourceGroup
 
 $disconnect = $diskVM | Where-Object {$_.DiskState -eq "Unattached"}
 $disconnect | ConvertTo-Json -Depth 5 | Set-Content -Path $resultOut -Encoding UTF8
